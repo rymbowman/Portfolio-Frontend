61e6ac2d3e9e7980dbcd3a1893beb8f851dbf1b2
Ryan Bowman <rymbowman@gmail.com>
2025-02-05 14:10:40 -0500

    set up routes for homepage and connectpage components

--- Portfolio-Frontend/src/App.jsx
+++ Portfolio-Frontend/src/App.jsx
@@ -1,9 +1,16 @@
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from "./pages/homepage/HomePage";
+import Connectpage from "./pages/connectpage/Connectpage";
 
 function App() {
-  
-
-  return 
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" Component={<Homepage />} />
+        <Route path="/contact" Component={<Connectpage />} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
