a7d341efff8e8f8907a29117d7c96630c265bc46
Ryan Bowman <rymbowman@gmail.com>
2025-02-05 14:02:20 -0500

    import 3 NavbarItems into the Navbar component that are links to homepage, resume, and connectpage

--- Portfolio-Frontend/src/components/nav/Navbar.jsx
+++ Portfolio-Frontend/src/components/nav/Navbar.jsx
@@ -1,7 +1,23 @@
 import "./Navbar.css";
+import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <NavbarItem link={"/"} iconImage={"bx bx-home-alt-2"} tooltip={"Home"} />
+      <NavbarItem
+        link={"https://rymbowman.github.io/Resume/"}
+        target="_blank"
+        iconImage={"bx bx-file"}
+        tooltip={"Resume"}
+      />
+      <NavbarItem
+        link={"/connect"}
+        iconImage={"bx bx-conversation"}
+        tooltip={"Connect"}
+      />
+    </div>
+  );
 };
 
 export default Navbar;
