e7df368a8f712690f6a863bd3806c7255a834f51
Ryan Bowman <rymbowman@gmail.com>
2025-02-05 13:57:11 -0500

    create NavbarItem link componenet with props being passed in

--- Portfolio-Frontend/src/components/nav/NavbarItem.jsx
+++ Portfolio-Frontend/src/components/nav/NavbarItem.jsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import "./Navbar.css";
 
-const NavbarItem = () => {
-  return <div>NavbarItem</div>;
+const NavbarItem = ({ link, target = "_self", iconImage, tooltip }) => {
+  return (
+    <div className="nav-item">
+      <Link
+        to={link}
+        target={target}
+        rel={target === "blank" ? "noopener noreferer" : "undefined"}
+      >
+        <i className={iconImage}></i>
+        <p className="nav-tooltip" onClick={(e) => e.preventDefault()}>
+          {tooltip}
+        </p>
+      </Link>
+    </div>
+  );
 };
 
+NavbarItem.propTypes = {
+  link: PropTypes.string,
+  target: PropTypes.string,
+  iconImage: PropTypes.string,
+  tooltip: PropTypes.string,
+};
 export default NavbarItem;
