6b36aec18c4c733dea9fcdc98a824c28b754ccb2
Ryan Bowman <rymbowman@gmail.com>
2025-02-05 14:17:25 -0500

    import Mainlayout component to wrap around the two main pages

--- Portfolio-Frontend/src/App.jsx
+++ Portfolio-Frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/HomePage";
 import Connectpage from "./pages/connectpage/Connectpage";
+import Mainlayout from "./layouts/Mainlayout";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" Component={<Homepage />} />
-        <Route path="/contact" Component={<Connectpage />} />
-      </Switch>
+      <Mainlayout>
+        <Switch>
+          <Route path="/" Component={<Homepage />} />
+          <Route path="/contact" Component={<Connectpage />} />
+        </Switch>
+      </Mainlayout>
     </Router>
   );
 }
