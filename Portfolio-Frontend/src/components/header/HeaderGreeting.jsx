ce6f204535479c27eeb35e53ecc6bcf45edc2a0c
Ryan Bowman <rymbowman@gmail.com>
2025-02-06 22:10:20 -0500

    create main intro headline and add styling in css file

---
+++ Portfolio-Frontend/src/components/header/HeaderGreeting.jsx
@@ -0,0 +1,14 @@
+const HeaderGreeting = () => {
+  return (
+    <div className="intro-container">
+      <p className="header-title" id="greeting">
+        Hello, my name is
+      </p>
+      <h1 className="header-title" id="name">
+        Ryan Bowman.
+      </h1>
+    </div>
+  );
+};
+
+export default HeaderGreeting;
