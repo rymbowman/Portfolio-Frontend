3b20c4f21dbe4a110fe432e1dd744e09e16b6746
Ryan Bowman <rymbowman@gmail.com>
2025-02-07 10:23:26 -0500

    import Form.jsx into Connectpage.jsx and import Connectpage.css file into Connectpage.jsx

---
+++ Portfolio-Frontend/src/pages/connectpage/Connectpage.jsx
@@ -0,0 +1,12 @@
+import Form from "../../components/connectForm/Form";
+import "./Connectpage.css";
+
+const Connectpage = () => {
+  return (
+    <div className="connectpage">
+      <Form />
+    </div>
+  );
+};
+
+export default Connectpage;
