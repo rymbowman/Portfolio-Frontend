dfe135ca654d7396dfcad043954b8f4273738c58
Ryan Bowman <rymbowman@gmail.com>
2025-02-07 07:47:25 -0500

    create about section intro component with necessary text

---
+++ Portfolio-Frontend/src/components/about/AboutIntro.jsx
@@ -0,0 +1,12 @@
+const AboutIntro = () => {
+  return (
+    <div id="about-section-intro">
+      <p>
+        Thanks for visiting! Trust is key to any relationship, so here is a bit
+        about me...
+      </p>
+    </div>
+  );
+};
+
+export default AboutIntro;
