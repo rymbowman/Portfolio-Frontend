33c55619edf1942cdd07a1727a91645bd432cfd0
Ryan Bowman <rymbowman@gmail.com>
2025-02-07 08:25:44 -0500

    fix route path for each image imported

--- Portfolio-Frontend/src/components/about/Slideshow.jsx
+++ Portfolio-Frontend/src/components/about/Slideshow.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import image1 from "../../assets/images/image1.jpg";
-import image2 from "../../assets/images/image2.jpg";
-import image3 from "../../assets/images/image3.jpg";
-import image4 from "../../assets/images/image4.jpg";
-import image5 from "../../assets/images/image5.jpg";
-import image6 from "../../assets/images/image6.jpg";
-import image7 from "../../assets/images/image7.jpg";
+import image1 from "../../assets/image1.jpg";
+import image2 from "../../assets/image2.jpg";
+import image3 from "../../assets/image3.jpg";
+import image4 from "../../assets/image4.jpg";
+import image5 from "../../assets/image5.jpg";
+import image6 from "../../assets/image6.jpg";
+import image7 from "../../assets/image7.jpg";
 
 const Slideshow = () => {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
