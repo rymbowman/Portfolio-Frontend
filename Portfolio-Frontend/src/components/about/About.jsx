0f6b5175614eaf2a01a6039e931951d7451dcaea
Ryan Bowman <rymbowman@gmail.com>
2025-02-07 08:06:40 -0500

    create AboutText.jsx file for the main writing of about section and export it to About.jsx

--- Portfolio-Frontend/src/components/about/About.jsx
+++ Portfolio-Frontend/src/components/about/About.jsx
@@ -1,10 +1,12 @@
 import AboutIntro from "./AboutIntro";
 import "./About.css";
+import AboutText from "./AboutText";
 
 const About = () => {
   return (
     <div className="about-container">
       <AboutIntro />
+      <AboutText />
     </div>
   );
 };
---
+++ Portfolio-Frontend/src/components/about/AboutText.jsx
@@ -0,0 +1,32 @@
+import "./About.css";
+
+const AboutText = () => {
+  return (
+    <div className="about-text-container">
+      <h3 className="about-headers">My Journey and Interests</h3>
+      <p className="about-content">
+        My Christian faith is central to my life, guiding my decisions and
+        purpose. I have been happily married to Carly since May 2022, and we
+        live near Philadelphia with our Golden Retriever, Brody, who joined our
+        family in 2024. I am passionate about traveling and Philadelphia sports.
+        As a former high school teacher, I have had the opportunity to explore
+        amazing places like Glacier National Park, the Great Wall of China, and
+        northern Thailand, which have deepened my appreciation for diverse
+        cultures.
+      </p>
+
+      <h3 className="about-headers">Why Software Development?</h3>
+      <p className="about-content">
+        I have always been drawn to creativity and logical problem-solving, so
+        when a friend suggested I explore software development, it felt like a
+        perfect fit. Since diving into the field, I have developed a passion for
+        debugging challenges and designing cutting-edge websites. I am inspired
+        by the vast opportunities for learning and growth in software
+        development and am eager to apply my skills to meaningful projects as I
+        continue to explore this dynamic industry.
+      </p>
+    </div>
+  );
+};
+
+export default AboutText;
