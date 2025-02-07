00efe713dce86a65d2e660b5ed8694bf714b9459
Ryan Bowman <rymbowman@gmail.com>
2025-02-07 07:54:26 -0500

    create About.jsx component as a main container for housing all the components in the about section of the portfolio

---
+++ Portfolio-Frontend/src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import AboutIntro from "./AboutIntro";
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <AboutIntro />
+    </div>
+  );
+};
+
+export default About;
