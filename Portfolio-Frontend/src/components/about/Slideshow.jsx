1e61fa1b277bd09886537664cf9dd96e3daa520f
Ryan Bowman <rymbowman@gmail.com>
2025-02-07 08:14:55 -0500

    import all images in assets folder and add them to slides array

--- Portfolio-Frontend/src/components/about/Slideshow.jsx
+++ Portfolio-Frontend/src/components/about/Slideshow.jsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import image1 from "../../assets/images/image1.jpg";
+import image2 from "../../assets/images/image2.jpg";
+import image3 from "../../assets/images/image3.jpg";
+import image4 from "../../assets/images/image4.jpg";
+import image5 from "../../assets/images/image5.jpg";
+import image6 from "../../assets/images/image6.jpg";
+import image7 from "../../assets/images/image7.jpg";
 
 const Slideshow = () => {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-  const slides = [image1];
+  const slides = [image1, image2, image3, image4, image5, image6, image7];
 
   const currentSlide = (n) => {
     let newSlideIndex = currentSlideIndex + n;
