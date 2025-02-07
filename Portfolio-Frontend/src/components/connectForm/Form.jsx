41e65ee821c3d82447eabaadd2864e7ac0742a48
Ryan Bowman <rymbowman@gmail.com>
2025-02-07 10:22:24 -0500

    create FormItem.jsx, Form.jsx, and Form.css files and import Form.css into each component

---
+++ Portfolio-Frontend/src/components/connectForm/Form.jsx
@@ -0,0 +1,7 @@
+import "./Form.css";
+
+const Form = () => {
+  return <div>Form</div>;
+};
+
+export default Form;
---
+++ Portfolio-Frontend/src/components/connectForm/FormItem.jsx
@@ -0,0 +1,6 @@
+import "./Form.css";
+const FormItem = () => {
+  return <div>FormItem</div>;
+};
+
+export default FormItem;
