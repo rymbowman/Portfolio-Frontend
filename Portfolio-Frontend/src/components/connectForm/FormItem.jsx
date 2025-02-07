5e5f2b07cec2cef940e116f0f914efad39b79fe8
Ryan Bowman <rymbowman@gmail.com>
2025-02-07 10:25:59 -0500

    create form item and the props that are passed with it

--- Portfolio-Frontend/src/components/connectForm/FormItem.jsx
+++ Portfolio-Frontend/src/components/connectForm/FormItem.jsx
@@ -1,6 +1,25 @@
+import PropTypes from "prop-types";
 import "./Form.css";
-const FormItem = () => {
-  return <div>FormItem</div>;
+const FormItem = ({ type, placeholder, name, id, className, rows }) => {
+  return (
+    <div className="form-row" id={id}>
+      <input
+        type={type}
+        className={className}
+        name={name}
+        placeholder={placeholder}
+        rows={rows}
+      ></input>
+    </div>
+  );
 };
 
+FormItem.propTypes = {
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  rows: PropTypes.number,
+  className: PropTypes.string,
+};
 export default FormItem;
