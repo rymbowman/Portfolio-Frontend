f6d83eaf3658827c4e945cc3425416c204b074fb
Ryan Bowman <rymbowman@gmail.com>
2025-02-07 09:30:19 -0500

    write jsx into Contacts.jsx to display each contact method

--- Portfolio-Frontend/src/components/contact/Contacts.jsx
+++ Portfolio-Frontend/src/components/contact/Contacts.jsx
@@ -1,7 +1,41 @@
+import { Link } from "react-router-dom";
 import "./Contacts.css";
 
 const Contacts = () => {
-  return <div>Contacts</div>;
+  return (
+    <div className="contact-methods">
+      <div className="contact-container">
+        <a
+          href="https://github.com/rymbowman"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="github"
+          className="contact-link"
+        >
+          <i className={"bx bxl-github contact-img"}></i>
+          <p className="contact-tooltip">Github</p>
+        </a>
+      </div>
+      <div className="contact-container">
+        <a
+          href="https://www.linkedin.com/in/ryanbowman24"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="LinkedIn"
+          className="contact-link"
+        >
+          <i className={"bx bxl-linkedin-square contact-img"}></i>
+          <p className="contact-tooltip">LinkedIn</p>
+        </a>
+      </div>
+      <div className="contact-container">
+        <Link to="/connect" id="email" className="contact-link">
+          <i className={"bx bx-envelope contact-img"}></i>
+          <p className="contact-tooltip">Email</p>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Contacts;
