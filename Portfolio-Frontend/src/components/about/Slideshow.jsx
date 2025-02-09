e0850d4c962e139094639f33a2d1cd86d9b3a8a4
Ryan Bowman <rymbowman@gmail.com>
2025-02-08 20:26:58 -0500

    update component spacing on homepage

--- Portfolio-Frontend/src/components/about/Slideshow.jsx
+++ Portfolio-Frontend/src/components/about/Slideshow.jsx
@@ -5,11 +5,10 @@ import image3 from "../../assets/image3.jpg";
 import image4 from "../../assets/image4.jpg";
 import image5 from "../../assets/image5.jpg";
 import image6 from "../../assets/image6.jpg";
-import image7 from "../../assets/image7.jpg";
 
 const Slideshow = () => {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-  const slides = [image1, image2, image3, image4, image5, image6, image7];
+  const slides = [image1, image2, image3, image4, image5, image6];
 
   const currentSlide = (n) => {
     let newSlideIndex = currentSlideIndex + n;
@@ -30,7 +29,7 @@ const Slideshow = () => {
     return () => {
       clearInterval(slideInterval);
     };
-  }, [currentSlideIndex]);
+  });
 
   return (
     <div className="slideshow-images-container">
